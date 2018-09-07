21c830921a184f85724b7b15b944a466e5cc524e
Matt Etress <mattetress@me.com>
2018-09-07 14:13:58 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,3 +32,14 @@ end
 def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?(board, index)
 end
+
+def turn(board)
+  puts "Please input 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board)
+    player_move(board, index, token)
+  else
+    turn(board)
+  end
+end
